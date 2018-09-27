0a9fc4676e73dccbd0d533319ebf4f277df26a72
PANJiawen <pjwadsn2@gmail.com>
2018-09-27 11:14:40 +0200

    first commit for github

---
+++ apiapps-semanticanalysis-connector/src/main/java/org/sesstim/quantim/apiapps/semanticanalysis/connector/domain/model/App.java
@@ -0,0 +1,13 @@
+package org.sesstim.quantim.apiapps.semanticanalysis.connector.domain.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ apiapps-store-connector/src/main/java/org/sesstim/quantim/apiapps/store/connector/domain/model/App.java
@@ -0,0 +1,13 @@
+package org.sesstim.quantim.apiapps.store.connector.domain.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
